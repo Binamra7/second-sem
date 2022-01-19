61cd8fee76520f4752e9d076b5f3af829bb3c68d
Binamra7 <binamra7khadka6@gmail.com>
2022-01-19 15:07:29 +0545

    lab 3 almost done

--- cpp/lab3/qns9.cpp
+++ cpp/lab3/qns9.cpp
@@ -1,4 +1,4 @@
-// Write a program to set a structure to hold feet and inches, assign values to the members of the structure and print out the values in the format 4’ 10” by function. Pass the structure to the function. [Note: if inch is greater than 12, covert it into feet]
+// Write a program to set a structure to hold feet and inches, assign values to the members of the structure and print out the values in the format 4’ 10” by function. Pass the structure to the function. [Note: if inch is greater than or equal 12, covert it into feet]
 
 #include <iostream>
 using namespace std;
@@ -10,7 +10,7 @@ struct height
 
 void display(height h)
 {
-    if (h.inches > 12)
+    if (h.inches >= 12)
     {
         h.feet = h.feet + (h.inches / 12);
         h.inches = h.inches % 12;
