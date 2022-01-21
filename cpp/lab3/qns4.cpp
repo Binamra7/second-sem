aa804dfa2de8545accc922c9323bdebf5c908765
Binamra7 <binamra7khadka6@gmail.com>
2022-01-21 11:53:42 +0545

    added qns 5

--- cpp/lab3/qns4.cpp
+++ cpp/lab3/qns4.cpp
@@ -11,29 +11,20 @@ int main()
     cin >> x >> y;
     high() = set;
     if (x == 100)
-    {
         cout << "Higher temperature = " << x << endl
              << "Lower temperature = " << y;
-    }
     else if (y == 100)
-    {
         cout << "Higher temperature = " << y << endl
              << "Lower temperature = " << x;
-    }
 }
 
 int &high()
 {
     if (x > y)
-    {
         return x;
-    }
     else if (x < y)
-    {
         return y;
-    }
     else
-    {
         cout << "equal temperature.";
-    }
+    return x;
 }
---
+++ cpp/lab3/qns5.cpp
@@ -0,0 +1,49 @@
+// Create a structure and pass this by reference to a function scale() and calculate the
+//  distance by using scaling factor. Display the distance in feet and inches using display()
+//  function. Write a main() program to exercise this function.
+
+#include <iostream>
+
+using namespace std;
+
+struct Distance
+{
+    int feet;
+    int inches;
+};
+
+void display(Distance d)
+{
+    cout << d.feet << " feet " << d.inches << " inches" << endl;
+}
+
+void convert(Distance &d)
+{
+    // d.inches += 12;
+    d.feet += d.inches / 12;
+    d.inches %= 12;
+    display(d);
+}
+
+// void scale(Distance &d, int scale_fac)
+// {
+//     d.feet = d.feet * scale_fac;
+//     d.inches = d.inches * scale_fac;
+//     convert(d);
+// }
+
+int main()
+{
+    int scaling_factor = 5;
+    Distance d;
+    // d = {5, 25};
+    int f, i;
+    cout << "Enter feet and inches: ";
+    cin >> f >> i;
+    d.feet = f;
+    d.inches = i;
+    // cout << "Distance before scaling: ";
+    // scale(d, scaling_factor);
+    convert(d);
+    return 0;
+}
