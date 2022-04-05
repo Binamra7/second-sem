e3fb040cb52b7a4516254a8dc8c70deed5cf5d6f
Binamra7 <binamra7khadka6@gmail.com>
2022-04-05 07:29:27 +0545

    created form

--- cpp/lab7/qns3.cpp
+++ cpp/lab7/qns3.cpp
@@ -1,41 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Counter
-{
-    int count;
-
-public:
-    Counter()
-    {
-        count = 7;
-    }
-    void display()
-    {
-        cout << "Count: " << count << endl;
-    }
-    Counter operator++()
-    {
-        Counter ans;
-        ans.count++;
-        return ans;
-    }
-    Counter operator++(int)
-    {
-        Counter a;
-        a.count++;
-        return a;
-    }
-};
-
 int main()
 {
-    Counter c1;
-    c1.display();
-    c1 = ++c1;
-    c1.display();
-    c1 = c1++;
-    c1.display();
+    int t, count;
+    string o, e;
+    cin >> t;
+    for (int cases = 1; cases <= t; cases++)
+    {
+        count = 0;
+        cin >> o >> e;
+        int len = o.length();
+        for (int i = 0; i < len; i++)
+        {
+            if (e.length() <= o.length())
+                break;
+            if (o[i] != e[i])
+            {
+                count++;
+                e.erase(e.begin() + i);
+                i--;
+            }
+        }
+        if (o == e)
+        {
+            printf("Case #%d: %d\n", cases, count);
+        }
+        else
+        {
+            printf("Case #%d: IMPOSSIBLE\n", cases);
+        }
+    }
     return 0;
 }
