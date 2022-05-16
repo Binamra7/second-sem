9db6993cb8388d945ee0cb1f3741b15e31ae00c1
Binamra Khadka <binamra7khadka6@gmail.com>
2022-05-16 21:04:51 +0545

    Update stack.cpp

--- cpp/stack.cpp
+++ cpp/stack.cpp
@@ -34,14 +34,10 @@ public:
 
 int main()
 {
-    // stack<int> s;
-    // s.push(10);
-    // s.push(20);
-    // s.push(30);
-    // s.push(40);
-    // s.push(50);
-
-
-
-    return 0;
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
 }
